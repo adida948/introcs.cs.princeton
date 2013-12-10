3ece89cc91d90840b492bc5a64ee1cb274bec6ad
Danilo <adida938@hotmail.com>
2013-12-09 22:39:22 -0500

    Ex 2.a/b

--- myProject/src/myProject/TenHelloWorlds.java
+++ myProject/src/myProject/TenHelloWorlds.java
@@ -2,9 +2,14 @@ package myProject;
 
 public class TenHelloWorlds {
 
-	public static void main(String[] args) {
+	// exercise 2.a Main method not public.
+	
+	// exercise 2.b 
+	// Exception in thread "main" java.lang.NoSuchMethodError: main
+	// Static: You don't have to create an object to its members(method or variable)
+	
+	public static  void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		for( int i= 0; i < 10; i++)
 		{
 			System.out.println("Hello World");
