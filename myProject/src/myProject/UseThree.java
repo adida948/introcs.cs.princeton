c26f0b0b806272d27277668d89b56dc14ff4fa07
DANILO_CARRION <adida948@hotmail.com>
2014-01-09 17:39:07 -0500

    // Exercise 1.1.5  completed. Signed-off-by: DANILO_CARRION <adida948@hotmail.com>

---
+++ myProject/src/myProject/UseThree.java
@@ -0,0 +1,31 @@
+package myProject;
+
+/*************************************************************************
+ *  Compilation:  javac UseThree.java
+ *  Execution:    java UseThree Dan Car Con
+ *
+ *  Takes 3 command-line arguments and prints them in reverse order
+ *  in a friendly greeting.
+ *
+ *  % java UseThree Alice Bob Carol
+ *  Hi, Carol, Bob, and Alice.
+ *
+ *  % java UseThree Carol Bob Alice
+ *  Hi, Alice, Bob, and Carol.
+ *
+ *************************************************************************/
+// Exercise 1.1.5   
+public class UseThree {
+
+    public static void main(String[] args) {
+        System.out.print("Hi, ");
+        System.out.print(args[2]);
+        System.out.print(", ");
+        System.out.print(args[1]);
+        System.out.print(", and ");
+        System.out.print(args[0]);
+        System.out.println(".");
+    }
+
+}
+
